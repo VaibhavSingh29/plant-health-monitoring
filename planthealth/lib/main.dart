27db2c775a2fd96f193479e4480c77c62bed77a8
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-04-19 23:07:14 +0530

    main

--- planthealth/lib/main.dart
+++ planthealth/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:planthealth/home.dart';
 import 'package:planthealth/splashscreen.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 void main() {
   runApp(MyApp());
 }
@@ -14,15 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: splashScreen(),
@@ -31,65 +25,54 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key key, this.title, this.plant_data}) : super(key: key);
 
   final String title;
+  final Map plant_data;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var plant_data=widget.plant_data;
+    var sensor_data = plant_data['data'];
+    var ideal_data = plant_data['ideal_data'];
+    String plant_name = plant_data['plant_name'];
+    String pot_id = plant_data['pot_id'];
+    String last_updated = plant_data['last_updated'];
+    List temperature_gauge = get_gauge_data(sensor_data['temperature'], ideal_data['temperature_range'], 0);
+    List light_gauge = get_gauge_data(sensor_data['light'], ideal_data['light_range'], 1);
+    List humidity_gauge = get_gauge_data(sensor_data['humidity'], ideal_data['humidity_range'], 2);
+    var suggesstions = plant_data['suggesstions'];
+
+    print(plant_data['suggesstions']);
+    
     return Scaffold(
         body: Container(
-      child: Stack(
-        children: [
-          Image.asset(
+
+          child: Stack(
+            children: [
+            Image.asset(
             'assets/images/background.jpg',
             width: double.infinity,
             height: double.infinity,
             fit: BoxFit.cover,
           ),
-          Center(
-            child: Column(
+            Center(
+              child: Column(
               children: [
                 SizedBox(height: 20),
-                Text('Plant Name',
+                Text(plant_name,
                     style: TextStyle(
                       fontSize: 30,
                       fontFamily: 'Righteous',
                     )),
-                Text('Status: Connected',
+                Text('Status:' + get_status(last_updated),
                     style: TextStyle(
                       fontSize: 15,
                       // fontFamily: 'Righteous',
@@ -99,7 +82,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   splashColor: Colors.black,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.all(Radius.circular(50))),
-                  onPressed: () {},
+                  onPressed: ()
+                  {
+                    remove_plant(pot_id);
+                  },
                   child: Padding(
                     padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
                     child: Text(
@@ -111,46 +97,157 @@ class _MyHomePageState extends State<MyHomePage> {
                 SizedBox(
                   height: 50,
                 ),
-                CircularPercentIndicator(
-                  animation: true,
-                  animationDuration: 1200,
-                  radius: 80.0,
-                  lineWidth: 7.0,
-                  percent: 0.90,
-                  circularStrokeCap: CircularStrokeCap.round,
-                  center: new Text("90%"),
-                  footer: Text("Moisture"),
-                  progressColor: Colors.green,
+                Text(suggesstions[3],
+                  style: TextStyle(
+                      fontSize: 20
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                //Temperature gauge
+                Row(
+                  children: [
+                    SizedBox(width: 20,),
+                    CircularPercentIndicator(
+
+                      animation: true,
+                      animationDuration: 1200,
+                      radius: 80.0,
+                      lineWidth: 7.0,
+                      percent: temperature_gauge[0].toDouble(),
+                      circularStrokeCap: CircularStrokeCap.round,
+                      center: new Text(temperature_gauge[1], style:
+                        TextStyle(
+                          fontSize: 20
+                        ),
+                      ),
+                      footer: Text("Temperature"),
+                      progressColor: Colors.redAccent,
+                    ),
+                    SizedBox(width: 20,),
+                    Text(suggesstions[0],
+                    style: TextStyle(
+                      fontSize: 20
+                    ),
+                    )
+                  ],
+                ),
+
+                SizedBox(height: 20),
+
+                //Humidity gauge
+                Row(
+                  children: [
+                    SizedBox(width: 20,),
+                    CircularPercentIndicator(
+                      animation: true,
+                      animationDuration: 1200,
+                      radius: 80.0,
+                      lineWidth: 7.0,
+                      percent: humidity_gauge[0].toDouble(),
+                      circularStrokeCap: CircularStrokeCap.round,
+                      center: new Text(humidity_gauge[1]),
+                      footer: Text("Humidity"),
+                      progressColor: Colors.blue[600],
+                    ),
+                    SizedBox(width: 20,),
+                    Text(suggesstions[1],
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
+                    )
+                  ],
                 ),
                 SizedBox(height: 20),
-                CircularPercentIndicator(
-                  animation: true,
-                  animationDuration: 1200,
-                  radius: 80.0,
-                  lineWidth: 7.0,
-                  percent: 0.5,
-                  circularStrokeCap: CircularStrokeCap.round,
-                  center: new Text("50%"),
-                  footer: Text("Temperature"),
-                  progressColor: Colors.yellow[600],
+
+                //Light gauge
+                Row(
+                  children: [
+                    SizedBox(width: 20,),
+                    CircularPercentIndicator(
+                      animation: true,
+                      animationDuration: 1200,
+                      radius: 80.0,
+                      lineWidth: 7.0,
+                      percent: light_gauge[0].toDouble(),
+                      circularStrokeCap: CircularStrokeCap.round,
+                      center: new Text(light_gauge[1]),
+                      footer: Text("Sunlight"),
+                      progressColor: Colors.yellowAccent,
+                    ),
+                    SizedBox(width: 20,),
+                    Text(suggesstions[2],
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
+                    )
+                  ],
                 ),
-                SizedBox(height: 20),
-                CircularPercentIndicator(
-                  animation: true,
-                  animationDuration: 1200,
-                  radius: 80.0,
-                  lineWidth: 7.0,
-                  percent: 0.3,
-                  circularStrokeCap: CircularStrokeCap.round,
-                  center: new Text("30%"),
-                  footer: Text("Sunlight"),
-                  progressColor: Colors.orange,
-                ),
+
               ],
             ),
           ),
         ],
       ),
     ));
+  }
+
+  String get_status(String last_updated)
+  {
+    if(last_updated=="0")
+      {
+        return "Inactive";
+      }
+    else
+      {
+        return "Active";
+      }
+
+  }
+
+  void remove_plant(String pot_id) async
+  {
+    SharedPreferences prefs= await SharedPreferences.getInstance();
+    String user_id = prefs.getString('user_id');
+    var url = Uri.parse('https://zlnhbt4ogh.execute-api.us-east-1.amazonaws.com/delete_plant');
+    var response= await http.post(url, body: '{"user_id": "$user_id", "pot_id": "$pot_id"}');
+    if(response.statusCode==200)
+    {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Plant deleted!")));
+      Navigator.pop(context);
+    }
+    else
+      {
+         ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Something went wrong, Plz try again!")));
+      }
+  }
+
+  List get_gauge_data(value, ideal_values, int i)
+  {
+    int val = int.parse(value);
+    print(value);
+    print(ideal_values);
+    var ideal_values_list = ideal_values.split("-");
+    int low = int.parse(ideal_values_list[0]);
+    int high = int.parse(ideal_values_list[1]);
+
+
+    if(val < low)
+    {
+      return [0, "❌"];
+    }
+    else if(val > high)
+    {
+      return [1, "❌"];
+    }
+    else
+    {
+      List gauge_valus=[];
+      double percent= (val - low )/ high;
+      gauge_valus.add(percent);
+      gauge_valus.add((percent * 100).toStringAsFixed(2));
+      return gauge_valus;
+    }
   }
 }
